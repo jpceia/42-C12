ab5ac10a821b6137d670c9dcb08a87e346eac373
jpceia <joao.p.ceia@gmail.com>
2021-12-05 11:22:18 +0000

    handling empty lists

--- ex04/ft_list_push_back.c
+++ ex04/ft_list_push_back.c
@@ -6,18 +6,24 @@
 /*   By: jpceia <joao.p.ceia@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/10 00:17:44 by jceia             #+#    #+#             */
-/*   Updated: 2021/12/05 01:11:09 by jpceia           ###   ########.fr       */
+/*   Updated: 2021/12/05 10:58:24 by jpceia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_list.h"
+#include <stdlib.h>
 
 void	ft_list_push_back(t_list **begin_list, void *data)
 {
 	t_list	*l;
 
 	l = *begin_list;
-	while (l->next)
-		l = l->next;
-	l->next = ft_create_elem(data);
+	if (l == NULL)
+		*begin_list = ft_create_elem(data);
+	else
+	{
+		while (l->next)
+			l = l->next;
+		l->next = ft_create_elem(data);
+	}
 }
