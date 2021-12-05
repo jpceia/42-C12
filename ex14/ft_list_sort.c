875a783b767fc84ae80d25d02037f6ceb636eb2c
jpceia <joao.p.ceia@gmail.com>
2021-12-05 11:40:34 +0000

    [fix] changing begin_list when the swap is in the first two nodes

--- ex14/ft_list_sort.c
+++ ex14/ft_list_sort.c
@@ -3,33 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   ft_list_sort.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jceia <marvin@42.fr>                       +#+  +:+       +#+        */
+/*   By: jpceia <joao.p.ceia@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/10 17:24:32 by jceia             #+#    #+#             */
-/*   Updated: 2020/12/10 17:42:43 by jceia            ###   ########.fr       */
+/*   Updated: 2021/12/05 11:33:01 by jpceia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_list.h"
+#include <stdlib.h>
 
-void	ft_list_sort(t_list **begin_list, int (*cmp)())
+void	ft_list_sort(t_list **begin_list, int (*cmp)(void *, void *))
 {
 	t_list	*l;
 	t_list	*l_prev;
 	t_list	*holder;
 
 	l = *begin_list;
-	l_prev = 0;
+	l_prev = NULL;
 	while (l->next)
 	{
 		if (cmp(l->data, l->next->data) < 0)
 		{
 			holder = l->next;
-			if (l_prev)
-				l_prev->next = l->next;
 			l->next = l->next->next;
 			holder->next = l;
+			if (l_prev)
+				l_prev->next = holder;
+			else
+				*begin_list = holder;
 			ft_list_sort(begin_list, cmp);
+			break ;
 		}
 		l_prev = l;
 		l = l->next;
