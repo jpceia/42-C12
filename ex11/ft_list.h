952f6e0dced5631001136a40e954b31ab2418faf
João Ceia <joao.p.ceia@gmail.com>
2021-12-25 14:11:16 +0000

    added header files

---
+++ ex11/ft_list.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpceia <joao.p.ceia@gmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/09 23:38:59 by jceia             #+#    #+#             */
+/*   Updated: 2021/12/05 01:07:48 by jpceia           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_LIST_H
+# define FT_LIST_H
+
+typedef struct s_list
+{
+	struct s_list	*next;
+	void			*data;
+}	t_list;
+
+t_list	*ft_create_elem(void *data);
+
+#endif
